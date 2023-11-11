a2f9ce8b456434c8b3ab948fc11645889281ccae
Samaresh Shil <108946101+samaresh96@users.noreply.github.com>
2023-11-11 05:29:38 +0000

    my 1st commit

---
+++ Main/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
+
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games; 
+    },
+    authors() {
+      return db.authors;
+    },
+
+    reviews() {
+      return db.reviews;
+    }
+  },
+};
+
+const server = new ApolloServer({
+  //typeDefs - different types of data
+  typeDefs,
+ resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server is ready at port", 4000);
---
+++ Main/module.js
@@ -0,0 +1,19 @@
+
+// const express = require("express")
+// const cluster = require("cluster")
+// const os = require("os")
+
+
+// const cpuNums = os.cpus().length
+
+// console.log(cpuNums)
+// var first = 10 
+// console.log(first)
+// var a = 0.1
+// var b = 0.2
+// var c = a+b
+// console.log(1 + 2 ===3)
+
+// function show(){
+
+// }
